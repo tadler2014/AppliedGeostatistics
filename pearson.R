299d7f40384c62d9e83a96d9e003006de2ca03cf
Thomas Adler <45015794+tadler2014@users.noreply.github.com>
2019-09-24 10:21:31 -0400

    Delete pearson.R

--- pearson.R
+++
@@ -1,8 +0,0 @@
-# Pearson Correlation Coefficient Function
-
-# Created by: Thomas Adler
-# Last Updated: September 11th, 2019
-
-pearson <- function(x,y){
-  r <- sum((x-mean(x))*(y-mean(y)))/sqrt(sum((x-mean(x))^2)*sum((y-mean(y))^2))
-}
